b7fd343bd6e8f785d8158b44725630843b43183e
井九 <wuhui921122@foxmail.com>
2018-12-21 23:08:55 +0800

    提交时间：2018-12-21 描述：将.yml文件改成.properties文件

--- api/src/main/java/com/springboot/api/controller/HelloController.java
+++ api/src/main/java/com/springboot/api/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.api.controller;
 
 import com.springboot.core.HelloService;
 import com.springboot.domain.entity.UserRole;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +20,15 @@ public class HelloController
 {
     @Autowired
     private HelloService helloService;
+    /**
+     * 添加日志
+     **/
+    private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public List<UserRole> hello() {
+        // 日志级别
+        LOG.debug("######  打印日志  #######");
         return helloService.sayHello();
     }
 }
